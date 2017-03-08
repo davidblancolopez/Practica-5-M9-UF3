03a781dea457b867121ab66b8f789578bc89d89e
davidblancolopez <daviddbl94@gmail.com>
2017-03-08 17:21:57 +0100

    Añadida QUERY en Client.

---
+++ src/practica/pkg5/m9/uf3/Practica5M9UF3.java
@@ -0,0 +1,12 @@
+
+package practica.pkg5.m9.uf3;
+
+
+public class Practica5M9UF3 {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/practica/pkg5/m9/uf3/Servidor.java
@@ -0,0 +1,7 @@
+
+package practica.pkg5.m9.uf3;
+
+
+public class Servidor {
+    
+}
